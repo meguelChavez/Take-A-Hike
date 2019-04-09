57b0db1f9d5918f4e954bbcbe76bcc9433e79e7e
Meguel <chavezmeguel@gmail.com>
2019-04-08 20:29:41 -0400

    wip content below searchbar

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -7,16 +7,20 @@ class HomePage extends Component {
     render() {
         return (
             <Container>
-                    <Row>
-                        <Col sm="12" md={{ size: 8, offset: 2 }}>
-                            <h1 id="searchLabel" className="align-middle">Find a Park Near You</h1>
-                        </Col>
-                    </Row>
-                    <Row>
-                        <Col sm="12" md={{ size: 8, offset: 2 }}>
-                            <SearchBar />
-                        </Col>
-                    </Row>
+                <Row>
+                    <Col sm="12" md={{ size: 8, offset: 2 }}>
+                        <h1 id="searchLabel" className="align-middle">Find a Park Near You</h1>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col sm="12" md={{ size: 8, offset: 2 }}>
+                        <SearchBar />
+                        <p className="lead">
+                            Get back to nature and search for parks near you. Find features, hours
+                            and directions.
+                        </p>
+                    </Col>
+                </Row>
             </Container>
         );
     }
