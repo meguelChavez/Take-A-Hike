07088dd1b9a3b83a406c69e7bd84029d048931ff
Meguel <chavezmeguel@gmail.com>
2019-04-09 00:27:45 -0400

    wip get route for nps api

--- client/src/components/searchBar.js
+++ client/src/components/searchBar.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import {
   InputGroup,
   InputGroupAddon,
@@ -22,6 +23,7 @@ class SearchBar extends Component {
       stateCode: ['AL', 'AK', 'AS', 'AZ', 'AR', 'CA', 'CO', 'CT', 'DE', 'DC', 'FM', 'FL', 'GA', 'GU', 'HI', 'ID', 'IL', 'IN', 'IA', 'KS', 'KY', 'LA', 'ME', 'MH', 'MD', 'MA', 'MI', 'MN', 'MS', 'MO', 'MT', 'NE', 'NV', 'NH', 'NJ', 'NM', 'NY', 'NC', 'ND', 'MP', 'OH', 'OK', 'OR', 'PW', 'PA', 'PR', 'RI', 'SC', 'SD', 'TN', 'TX', 'UT', 'VT', 'VI', 'VA', 'WA', 'WV', 'WI', 'WY'],
       chosenSC: "",
       keyword: "",
+      parks: [],
       dropdownOpen: false,
       splitButtonOpen: false
     };
@@ -42,26 +44,40 @@ class SearchBar extends Component {
   handleClick = (event) => {
     console.log(event);
 
-  } 
+  }
 
   handleInputChange = (event) => {
-    console.log(this);
-    console.log(event.target)
     const { name, value } = event.target;
     this.setState({
       [name]: value
     });
   };
 
+  handleSearch = (event) => {
+    const { keyword, chosenSC } = this.state;
+    console.log("clicked");
+    console.log(keyword, chosenSC)
+    axios.get("/search", {
+      params: {
+        keyword,
+        chosenSC
+      }
+    }).then((res) => {
+      this.setState({ parks: res.data.data });
+      console.log(res.data.data);
+    }).catch((err) => {
+      console.log(err);
+    })
+  }
 
   render() {
     const { stateCode } = this.state;
     return (
       <div>
         <InputGroup>
-          <Input 
+          <Input
             value={this.state.keyword}
-            name="keyword" 
+            name="keyword"
             onChange={this.handleInputChange}
             placeholder="Keyword (required)"
           />
@@ -69,7 +85,7 @@ class SearchBar extends Component {
             <DropdownToggle caret>
               {this.state.chosenSC ? this.state.chosenSC : "State"}
             </DropdownToggle>
-            <DropdownMenu 
+            <DropdownMenu
               modifiers={{
                 setMaxHeight: {
                   enabled: true,
@@ -92,7 +108,7 @@ class SearchBar extends Component {
               ))}
             </DropdownMenu>
           </InputGroupButtonDropdown>
-          <Button className="ml-3">Search</Button>
+          <Button className="ml-3" onClick={this.handleSearch}>Search</Button>
 
         </InputGroup>
       </div>
--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,4 +1,6 @@
-let path = require("path");
+const path = require("path");
+const axios = require("axios");
+const keys = require("../keys/keys");
 
 module.exports = (app) => {
     // app.get("/", (req, res) => {
@@ -6,15 +8,25 @@ module.exports = (app) => {
     //     res.sendFile(path.join(__dirname, "/../public/index.html"));
     // });
 
-    app.get("/search", (req,res) =>{
-        console.log("search");
-        // console.log(req.query);
-        console.log(req.query);
-        const {query, stateCode} = req.query;
-        console.log(`q ${query} sc ${stateCode}`)
-        res.json("{success: 200}");
+    app.get("/search", (req, res) => {
+        const { keyword, chosenSC } = req.query;
+        const key = keys.natParkService.api_key
+        const fields = "fields=addresses%2Cimages%2Ccontacts%2CweatherInfo%2CentranceFees%2CoperatingHours"
+        const queryUrl = `https://developer.nps.gov/api/v1/parks?stateCode=${chosenSC}&q=${keyword}&${fields}&api_key=${key}`;
+        axios.get(queryUrl).then((response) => {
+            console.log(response.data.data);
+            res.json(response.data);
+        }).catch((err) => {
+            console.log(err)
+        });
+        // res.json("{success: 200}");
     })
-    // app.get("*", (req, res) =>{
-    //     res.sendFile(path.join(__dirname, "/../public/index.html"));
+    // app.get("*", (req, res) => {
+    //     console.log("search");
+    //     console.log(req.params);
+    //     console.log(req.query);
+    //     const { query, stateCode } = req.query;
+    //     console.log(`q ${query} sc ${stateCode}`)
+    //     res.send("{success: 200}");
     // });
 }
