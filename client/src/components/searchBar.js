b99d81faf95e2a92159efb76238cb2fc4fb7d13a
Meguel <chavezmeguel@gmail.com>
2019-04-10 23:45:41 -0400

    updating bootstraps classNames and wip

--- client/src/components/searchBar.js
+++ client/src/components/searchBar.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import axios from 'axios';
 import {
   InputGroup,
   InputGroupAddon,
@@ -20,10 +19,6 @@ class SearchBar extends Component {
     this.toggleDropDown = this.toggleDropDown.bind(this);
     this.toggleSplit = this.toggleSplit.bind(this);
     this.state = {
-      stateCode: ['AL', 'AK', 'AS', 'AZ', 'AR', 'CA', 'CO', 'CT', 'DE', 'DC', 'FM', 'FL', 'GA', 'GU', 'HI', 'ID', 'IL', 'IN', 'IA', 'KS', 'KY', 'LA', 'ME', 'MH', 'MD', 'MA', 'MI', 'MN', 'MS', 'MO', 'MT', 'NE', 'NV', 'NH', 'NJ', 'NM', 'NY', 'NC', 'ND', 'MP', 'OH', 'OK', 'OR', 'PW', 'PA', 'PR', 'RI', 'SC', 'SD', 'TN', 'TX', 'UT', 'VT', 'VI', 'VA', 'WA', 'WV', 'WI', 'WY'],
-      chosenSC: "",
-      keyword: "",
-      parks: [],
       dropdownOpen: false,
       splitButtonOpen: false
     };
@@ -41,49 +36,20 @@ class SearchBar extends Component {
     });
   }
 
-  handleClick = (event) => {
-    console.log(event);
-
-  }
-
-  handleInputChange = (event) => {
-    const { name, value } = event.target;
-    this.setState({
-      [name]: value
-    });
-  };
-
-  handleSearch = (event) => {
-    const { keyword, chosenSC } = this.state;
-    console.log("clicked");
-    console.log(keyword, chosenSC)
-    axios.get("/search", {
-      params: {
-        keyword,
-        chosenSC
-      }
-    }).then((res) => {
-      this.setState({ parks: res.data.data });
-      console.log(res.data.data);
-    }).catch((err) => {
-      console.log(err);
-    })
-  }
-
   render() {
     const { stateCode } = this.state;
     return (
       <div>
         <InputGroup>
           <Input
-            value={this.state.keyword}
+            value={this.props.keyword}
             name="keyword"
-            onChange={this.handleInputChange}
+            onChange={this.props.handleInputChange}
             placeholder="Keyword (required)"
           />
           <InputGroupButtonDropdown addonType="append" isOpen={this.state.dropdownOpen} toggle={this.toggleDropDown}>
-            <DropdownToggle caret>
-              {this.state.chosenSC ? this.state.chosenSC : "State"}
+            <DropdownToggle className="btn-success" caret>
+              {this.props.chosenSC ? this.props.chosenSC : "State"}
             </DropdownToggle>
             <DropdownMenu
               modifiers={{
@@ -103,12 +69,12 @@ class SearchBar extends Component {
                 },
               }}
             >
-              {stateCode.map((element, index) => (
-                < DropdownItem key={index} name="chosenSC" value={element} onClick={this.handleInputChange} >  {element} </DropdownItem>
+              {this.props.stateCode.map((element, index) => (
+                < DropdownItem key={index} name="chosenSC" value={element} onClick={this.props.handleInputChange} >  {element} </DropdownItem>
               ))}
             </DropdownMenu>
           </InputGroupButtonDropdown>
-          <Button className="ml-3" onClick={this.handleSearch}>Search</Button>
+          <Button className="ml-3 btn-success" onClick={this.props.handleSearch}>Search</Button>
 
         </InputGroup>
       </div>
--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -1,10 +1,52 @@
 import React, { Component } from 'react';
 import { Container, Row, Col, Button } from 'reactstrap';
+import axios from 'axios';
 import SearchBar from '../components/searchBar';
+import ParkCard from '../components/parkCard';
+
 
 
 class HomePage extends Component {
+    state = {
+        stateCode: ['AL', 'AK', 'AS', 'AZ', 'AR', 'CA', 'CO', 'CT', 'DE', 'DC', 'FM', 'FL', 'GA', 'GU', 'HI', 'ID', 'IL', 'IN', 'IA', 'KS', 'KY', 'LA', 'ME', 'MH', 'MD', 'MA', 'MI', 'MN', 'MS', 'MO', 'MT', 'NE', 'NV', 'NH', 'NJ', 'NM', 'NY', 'NC', 'ND', 'MP', 'OH', 'OK', 'OR', 'PW', 'PA', 'PR', 'RI', 'SC', 'SD', 'TN', 'TX', 'UT', 'VT', 'VI', 'VA', 'WA', 'WV', 'WI', 'WY'],
+        chosenSC: "",
+        keyword: "",
+        parks: []
+    }
+
+    // getParks = (parkArr) => {
+    //     this.setState({ parks: parkArr });
+    // }
+
+    handleInputChange = (event) => {
+        const { name, value } = event.target;
+        this.setState({
+            [name]: value
+        });
+    };
+
+    handleSearch = (event) => {
+        console.log(this.props);
+        const { keyword, chosenSC } = this.state;
+        console.log("clicked");
+        console.log(keyword, chosenSC)
+        axios.get("/search", {
+            params: {
+                keyword,
+                chosenSC
+            }
+        }).then((res) => {
+            const parkArr = res.data.data;
+            //   this.props.getParks(parkArr)
+            this.setState({ parks: res.data.data });
+            console.log(res.data.data);
+        }).catch((err) => {
+            console.log(err);
+        })
+    }
+
     render() {
+        const { stateCode, keyword, chosenSC } = this.state;
         return (
             <Container>
                 <Row>
@@ -14,12 +56,25 @@ class HomePage extends Component {
                 </Row>
                 <Row>
                     <Col sm="12" md={{ size: 8, offset: 2 }}>
-                        <SearchBar />
+                        <SearchBar
+                            chosenSC={chosenSC}
+                            keyword={keyword}
+                            stateCode={stateCode}
+                            getParks={this.getParks}
+                            handleInputChange={this.handleInputChange}
+                            handleSearch={this.handleSearch}
+                        />
                         <p className="lead">
                             Get back to nature and search for parks near you. Find features, hours
                             and directions.
                         </p>
                     </Col>
+                </Row>
+                <Row className="parks">
+                    {this.state.parks ?
+                        this.state.parks.map((element, i) => (
+                            <ParkCard key={i} park={this.state.parks[i]} />))
+                        : ""}
                 </Row>
             </Container>
         );
