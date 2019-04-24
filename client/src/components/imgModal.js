8b4d1cfe01e544733b5aadae3c1e013739c5b8df
Meguel <chavezmeguel@gmail.com>
2019-04-23 23:45:15 -0400

    resized img modal

--- client/src/components/imgModal.js
+++ client/src/components/imgModal.js
@@ -21,7 +21,7 @@ class ModalExample extends React.Component {
         return (
             <div className="col-md-3">
                 <img alt={this.props.images.alt} onClick={this.toggle} className="infoImg d-flex justify-content-between mb-3" src={this.props.images.url} />
-                <Modal className="col-md-12" isOpen={this.state.modal} modalTransition={{ timeout: 700 }} backdropTransition={{ timeout: 1300 }}
+                <Modal size="lg" isOpen={this.state.modal} modalTransition={{ timeout: 700 }} backdropTransition={{ timeout: 1300 }}
                     toggle={this.toggle}>
                     <ModalHeader toggle={this.toggle}>{this.props.images.caption}</ModalHeader>
                     <img alt={this.props.images.alt} onClick={this.toggle} className="imgModal" src={this.props.images.url} />
